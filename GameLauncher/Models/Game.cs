f82f84b6549e927d1bc8171a23d4e5e12d935404
Ethan H. <neyteck.pro@hotmail.com>
2025-07-08 15:32:43 +0200

    Push: game_default profile picture Asset and Note Model class ; Edit: All models code (User, Game and Note) is written with DB preparation (may be edit in future)

--- GameLauncher/Models/Game.cs
+++ GameLauncher/Models/Game.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameLauncher.Models
 {
-    internal class Game
+    public class Game
     {
+        #region Game Properties
+        [Key]
+        public int Id { get; set; }
+        public string GameName { get; set; } = string.Empty;
+        public string GameDescription { get; set; } = string.Empty;
+        public string GameProfilePicture { get; set; } = "game_default.png";
+
+        // Game added date
+        public DateTime GameAddedDate { get; set; } = DateTime.Now;
+
+        public bool GameOwned { get; set; }
+        public string GameLaunchPath { get; set; } = string.Empty;
+
+        public List<Note> GameNoteList { get; set; } = [];
+
+        #endregion
+        // Empty Constructor
+        public Game() { }
+
+        // Creation Contructor for DB insertion and Shelf Game Creation
+        public Game(string gamename, string gamedescription)
+        {
+            GameName = gamename;
+            GameDescription = gamedescription;
+        }
     }
 }
---
+++ GameLauncher/Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncher.Models
+{
+    public class Note
+    {
+        #region Note Properties
+        [Key]
+        public int Id { get; set; }
+        public string NoteTitle { get; set; } = string.Empty;
+        public string NoteContent { get; set; } = string.Empty;
+        // Creation and Lasted edit Date
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime LastedEditTimeDate { get; set; }
+
+        #endregion
+        // Empty Constructor
+        public Note() { }
+
+        public Note(string  noteTitle, string noteContent)
+        {
+            NoteTitle = noteTitle;
+            NoteContent = noteContent;
+            LastedEditTimeDate = DateTime.Now;
+        }
+    }
+}
